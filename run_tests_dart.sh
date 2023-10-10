96d947c16b0e204516f0f5cb9623119983028365
Luisa Coelho <luisa@omnicode.solutions>
2023-10-10 09:43:23 -0300

    fix: gh-actions fail when tests dont pass

--- run_tests_dart.sh
+++ run_tests_dart.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+set -euo pipefail
+
 cd dart/
 
 for exercise in *; do 
 [ -d $exercise ] && echo "Testing $exercise"; 
 cd $exercise; 
 dart pub get;
-dart format --output=none --set-exit-if-changed .;
 dart analyze;
 dart test;
 cd ..;
